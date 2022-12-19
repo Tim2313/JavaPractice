cb6105f70f5fbec8946b393f104e22071e65438a
Tima Kishchuk <tkisuk@gmail.com>
2022-12-19 20:16:14 +0200

    [Sound] Implement task from book

---
+++ src/Sound.java
@@ -0,0 +1,9 @@
+class Sound {
+	public static void main (String args []) {
+		double dist;
+			
+		dist = 1100 * 7.2;
+		
+		System.out.println("Расстояние до место вспышки молнии " + "составляет " + dist + " футов");
+	}	
+}	
