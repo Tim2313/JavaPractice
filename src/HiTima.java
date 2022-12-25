31a8140ca916988047d3e3a05e36f9b5a30eab2b
Tima Kishchuk <tkisuk@gmail.com>
2022-12-25 20:30:55 +0200

    ./src/HiTima.java

--- src/HiTima.java
+++ src/HiTima.java
@@ -2,21 +2,7 @@ public class HiTima {
 	public static void main(String[] args) throws java.io.IOException {
 		System.out.println("Введіть своє ім'я: ");
 
-		// for (int i = 0; i < 100; i++) {
-		// 	char a = (char) System.in.read();
-		// 	System.out.println("Char: " + a);
-		// }
-
 		String name = "";
-
-		// do {
-		// 	a = (char) System.in.read();
-
-		// 	if (a != '\n') {
-		// 		name = name + a;
-		// 	}
-
-		// } while (a != '\n');
 
 		char a = 'a';
 		for (;a != '\n';) {
@@ -25,10 +11,9 @@ public class HiTima {
 			if (a != '\n') {
 			 	name += a;
 			}
-
-			//name += a;
 		}
 
 		System.out.println("Hi " + name + "!");
 	}
 }
+
