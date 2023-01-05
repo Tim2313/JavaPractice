da776509a34db0c4a03d7183baf42dab693a7e16
Tima Kishchuk <tkisuk@gmail.com>
2023-01-05 19:01:15 +0200

    [JavaArgs]Args + 1

---
+++ src/NumberPlus.java
@@ -0,0 +1,20 @@
+public class NumberPlus {
+	public static void main(String[] args) {
+
+		if (args.length != 1) {
+			int count = args.length;
+			System.out.println("Invalid Input. Arguments count: " + count);
+			return;
+		} 
+
+		int numberP = Integer.parseInt(args[0]);
+		String res = "Result ";
+		int operate = numberP + 1;
+		String fin = res + operate;
+		System.out.println(fin);
+
+	}
+}
+
+
+
